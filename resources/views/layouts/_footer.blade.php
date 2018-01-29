501e961cb916ed5832bd2fa619afa82fa42b87c5
chenbaojin <529834149@qq.com>
2018-01-29 13:21:46 +0800

    no message

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://www.chen2016php.com" target="_blank">Chenbj</a>编程|算法 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="chen2018php@163.com">联系我们</a></p>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
  Route::get('demo','DemoController@index');
+ Route::get('/', 'PagesController@root')->name('root');
