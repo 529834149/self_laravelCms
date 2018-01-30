1da6d9a79f1180ba767a41cf645e77052f33527e
chenbaojin <529834149@qq.com>
2018-01-30 16:47:04 +0800

    no message

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,8 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password')->nullable();;
-            $table->string('avatar')->nullable();;
+            $table->string('password')->nullable();
+            $table->string('avatar');
             $table->string('introduction');
             $table->rememberToken();
             $table->timestamps();
